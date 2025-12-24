e4d9fed98b2f05064cc44e0430b04d882a7dbb7a
Sven Linfoot <svenlinfoot@gmail.com>
2025-12-24 18:20:02 +0000

    use deployed url on render

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 const CONFIG = {
   // Update this URL when deploying to production
-  API_BASE_URL: 'http://localhost:3000/api/routes'
+  // API_BASE_URL: 'http://localhost:3000/api/routes'
+  API_BASE_URL: 'https://track-diagrams-server.onrender.com/api/routes'
 };
